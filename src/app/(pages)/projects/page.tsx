326d979e70f86e69070561fe4a697a6c252ee43c
Aiyanu <96719103+Aiyanu@users.noreply.github.com>
2023-06-18 20:36:09 +0100

    added grayscale

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -8,18 +8,7 @@ export default function Projects() {
       <h1 className="text-4xl uppercase ml-4 mb-8">Projects</h1>
       <div className="flex flex-col lg:flex-row items-center gap-12 justify-around p-4">
         <Link
-          className={` bg-no-repeat border-4 border-[#bbb] rounded-md w-72 h-96 bg-cover relative hover:text-[#4b6cc1]`}
-          href={"$"}
-          style={{
-            backgroundImage: `url("/apps.jpg")`,
-          }}
-        >
-          <span className="absolute right-2 bottom-2 text-4xl font-bold">
-            Blog
-          </span>
-        </Link>
-        <Link
-          className={`bg-[/website.jpg] border-4 border-[#bbb] rounded-md w-72 h-96 bg-cover relative hover:text-[#4b6cc1]`}
+          className={` grayscale hover:grayscale-0 border-4 border-[#bbb] rounded-md w-72 h-96 bg-cover relative hover:text-[#4b6cc1]`}
           href={"$"}
           style={{
             backgroundImage: `url("/illustration.png")`,
@@ -30,10 +19,21 @@ export default function Projects() {
           </span>
         </Link>
         <Link
-          className={` bg-no-repeat border-4 border-[#bbb] rounded-md w-72 h-96 bg-cover relative hover:text-[#4b6cc1]`}
+          className={`bg-[/website.jpg] grayscale hover:grayscale-0 border-4 border-[#bbb] rounded-md w-72 h-96 bg-cover relative hover:text-[#4b6cc1]`}
           href={"$"}
           style={{
-            backgroundImage: `url("/websites.jpg")`,
+            backgroundImage: `url("/illustration.png")`,
+          }}
+        >
+          <span className="absolute right-2 bottom-2 text-4xl font-bold">
+            Blog
+          </span>
+        </Link>
+        <Link
+          className={` bg-no-repeat grayscale hover:grayscale-0 border-4 border-[#bbb] rounded-md w-72 h-96 bg-cover relative hover:text-[#4b6cc1]`}
+          href={"$"}
+          style={{
+            backgroundImage: `url("/illustration.png")`,
           }}
         >
           <span className="absolute right-2 bottom-2 text-4xl font-bold">
