bf63582c08fa5e4ec86077d094a6372ec09df048
Iyanuoluwapo Adesanya <aiyanu1.00@gmail.com>
2023-07-12 14:35:14 +0100

    fixed layouts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,27 +7,27 @@ import Link from "next/link";
 import { rubik } from "@/app/layout";
 
 export default function Header() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  // const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
-  // update state on toggle
-  const handleToggle = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
+  // // update state on toggle
+  // const handleToggle = (e) => {
+  //   if (e.target.checked) {
+  //     setTheme("dark");
+  //   } else {
+  //     setTheme("light");
+  //   }
+  // };
 
-  // set theme state in localstorage on mount & also update localstorage on state change
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const localTheme = localStorage?.getItem("theme");
-    // add custom data-theme attribute to html tag required to update theme using DaisyUI
-    if (localTheme) {
-      // add custom data-theme attribute to html tag required to update theme using DaisyUI
-      document.querySelector("html")?.setAttribute("data-theme", localTheme);
-    }
-  }, [theme]);
+  // // set theme state in localstorage on mount & also update localstorage on state change
+  // useEffect(() => {
+  //   localStorage.setItem("theme", theme);
+  //   const localTheme = localStorage?.getItem("theme");
+  //   // add custom data-theme attribute to html tag required to update theme using DaisyUI
+  //   if (localTheme) {
+  //     // add custom data-theme attribute to html tag required to update theme using DaisyUI
+  //     document.querySelector("html")?.setAttribute("data-theme", localTheme);
+  //   }
+  // }, [theme]);
   return (
     <header
       className={
@@ -48,23 +48,23 @@ export default function Header() {
         <div>
           <h1 className=" text-4xl flex item-center gap-8">
             <Link href={"/"}>AIYANU</Link>
-            <div className="flex-none">
+            {/* <div className="flex-none"> */}
               {/* Toggle button here */}
-              <button className="btn btn-square btn-ghost">
+              {/* <button className="btn btn-square btn-ghost">
                 <label className="swap swap-rotate w-12 h-12">
                   <input
                     type="checkbox"
                     onChange={handleToggle}
                     // show toggle image based on localstorage theme
                     checked={theme === "light" ? false : true}
-                  />
+                  /> */}
                   {/* light theme sun image */}
-                  <p className="w-8 h-8 swap-on">☀️</p>
+                  {/* <p className="w-8 h-8 swap-on">☀️</p> */}
                   {/* dark theme moon image */}
-                  <p className="w-8 h-8 swap-off">🌙</p>
+                  {/* <p className="w-8 h-8 swap-off">🌙</p>
                 </label>
               </button>
-            </div>
+            </div> */}
           </h1>
           <div className=" rounded-md w-9 h-2 bg-[#4B6CC1]" />
           <div className="lg:block hidden">
